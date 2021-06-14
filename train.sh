6fc793ceb4b723b4ea1dc80e20278beb8f491c77
Sevag Hanssian <sevagh@protonmail.com>
2021-06-14 17:18:35 -0400

    Full training

--- train.sh
+++ train.sh
@@ -8,22 +8,12 @@ epochs=1000
 workers=4
 seqdur=1
 
-# best drum config for sllen <= 2048
-#python scripts/train.py \
-#	--root "${musdbdir}" --is-wav --nb-workers=$workers --batch-size=$batch --epochs=$epochs \
-#	--seq-dur=$seqdur --valid-seq-dur=$seqdur --valid-samples-per-track=64 --valid-batch-size=$batch \
-#	--samples-per-track=1 --fixed-start \
-#	--target="vocals" \
-#	--fscale='mel' --fbins=116 --fmin=37.7 --sllen=8024 \
-#	--output "${outdir}" --patience=1000 --debug
-
 python scripts/train.py \
 	--root "${musdbdir}" --is-wav --nb-workers=$workers --batch-size=$batch --epochs=$epochs \
 	--seq-dur=$seqdur --valid-seq-dur=$seqdur --valid-samples-per-track=64 --valid-batch-size=$batch \
-	--samples-per-track=1 --fixed-start \
-	--target="drums" \
-	--fscale='mel' --fbins=104 --fmin=49.3 --sllen=7108 \
-	--output "${outdir}" --patience=1000 --debug
+	--target="vocals" \
+	--fscale='mel' --fbins=116 --fmin=37.7 --sllen=8024 \
+	--output "${outdir}"
 
 #evaldir=$outdir
 
