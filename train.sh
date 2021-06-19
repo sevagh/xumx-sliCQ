8835e140d892abb09490df5aff9e42073af46e9a
Sevag Hanssian <sevagh@protonmail.com>
2021-06-19 14:50:58 -0400

    LSTM size back down to 512x3
    Improve training script

--- train.sh
+++ train.sh
@@ -4,18 +4,24 @@ musdbdir="/home/sevagh/TRAINING-MUSIC/MUSDB18-HQ/"
 musdbdebug="/home/sevagh/musdbdebug"
 outdir="umx-slicq-1"
 
-batch=8
+batch=32
 epochs=1000
 workers=4
-seqdur=5
+seqdur=6
 
-python scripts/train.py \
-	--root "${musdbdir}" --is-wav --nb-workers=$workers --batch-size=$batch --epochs=$epochs \
-	--seq-dur=$seqdur --samples-per-track=16 \
-	--target="vocals" --fscale='mel' --fbins=116 --fmin=37.7 --sllen=8024 \
-	--output "${outdir}" --skip-statistics \
-	--source-augmentations gain channelswap
+declare -a targetargs=(
+	"--target=vocals --fscale=mel --fbins=116 --fmin=37.7 --sllen=8024"
+	"--target=drums --fscale=mel --fbins=104 --fmin=49.3 --sllen=7108"
+	"--target=other --fscale=bark --fbins=64 --fmin=90.0 --sllen=4416"
+	"--target=bass --fscale=bark --fbins=105 --fmin=25.4 --sllen=7180"
+)
 
-	#--target="drums" --fscale='mel' --fbins=104 --fmin=49.3 --sllen=7108 \
-	#--target="other" --fscale='bark' --fbins=64 --fmin=90.0 --sllen=4416 \
-	#--target="bass" --fscale='bark' --fbins=105 --fmin=25.4 --sllen=7180 \
+for i in "${targetargs[@]}"
+do
+	python scripts/train.py \
+		--root "${musdbdir}" --is-wav --nb-workers=$workers --batch-size=$batch --epochs=$epochs \
+		--seq-dur=$seqdur \
+		$i --skip-statistics \
+		--output "${outdir}" \
+		--source-augmentations gain channelswap
+done
